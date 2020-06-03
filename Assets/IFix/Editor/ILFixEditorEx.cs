880e3dfac6ac40ac7c866c6b4d528ca7bd538bb1
huanghanzhi <274962828@qq.com>
2020-06-03 09:20:38 +0800

    增加override测试，增加快速转换热修代码和源代码的切换宏

---
+++ Assets/IFix/Editor/ILFixEditorEx.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Build.Player;
+using UnityEngine;
+
+public class ILFixEditorEx
+{
+    public const string PATCH_DEFINE = "PATCH_VERSION";
+
+    [MenuItem("InjectFix/SwitchToPatchMode")]
+    public static void SwitchPatchMode()
+    {
+        string defineSymbolStr = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone);
+        string[] defineSymbols = defineSymbolStr.Split(';');
+
+        bool isPatchMode = false;
+        foreach (string defineSymbo in defineSymbols)
+        {
+            if (defineSymbo == PATCH_DEFINE)
+            {
+                isPatchMode = true;
+            }
+        }
+
+        if (!isPatchMode)
+        {
+            if (string.IsNullOrEmpty(defineSymbolStr))
+            {
+                defineSymbolStr += PATCH_DEFINE;
+            }
+            else
+            {
+                defineSymbolStr = PATCH_DEFINE + ";" + defineSymbolStr;
+            }
+        }
+        else
+        {
+            defineSymbolStr = defineSymbols.Length == 1 ? "" : defineSymbolStr.Substring(defineSymbolStr.IndexOf(';'));
+        }
+
+        SwitchMode(!isPatchMode, defineSymbolStr);
+    }
+
+    public static void SwitchMode(bool isPatch, string newSymbols)
+    {
+        Menu.SetChecked("InjectFix/SwitchToPatchMode", isPatch);
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, newSymbols);
+
+        var outputDir = "Temp/ifix";
+        Directory.CreateDirectory("Temp");
+        Directory.CreateDirectory(outputDir);
+        ScriptCompilationSettings scriptCompilationSettings = new ScriptCompilationSettings();
+        scriptCompilationSettings.group = BuildTargetGroup.Standalone;
+        scriptCompilationSettings.target = BuildTarget.StandaloneWindows;
+        PlayerBuildInterface.CompilePlayerScripts(scriptCompilationSettings, outputDir);
+    }
+}
